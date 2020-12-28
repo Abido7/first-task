64423e421e795cf1950a8d98c662e3e8581a23b4
Ahmed-Magdyyy <76597137+Ahmed-Magdyyy@users.noreply.github.com>
2020-12-28 20:10:57 +0200

    updating the project js file

--- js/main.js
+++ js/main.js
@@ -1,4 +1,44 @@
-let firstName = document.getElementById("name");
-setTimeout(function () {
-  firstName.style.display = "block";
-}, 2000);
+let btn = document.getElementById("btn");
+let demo = document.getElementById("demo");
+let author = document.getElementById("author")
+
+let sayings = []
+
+let magdy = {
+    body: "“How wonderful it is that nobody need wait a single moment before starting to improve the world.”",
+    author: "– Anne Frank"
+}
+sayings.push(magdy)
+
+let mahmoud = {
+    body: "mahmoud nageh",
+  author: "– Anne Frank",
+}
+sayings.push(mahmoud)
+
+let yehia = {
+    body: "yehia tarek",
+    author: "– Anne Frank",
+}
+sayings.push(yehia)
+
+let hussein = {
+    body: "mohamed hussein",
+    author: "– Anne Frank",
+}
+sayings.push(hussein)
+
+let heba = {
+    body: "heba nagib",
+    author: "– Anne Frank",
+}
+sayings.push(heba)
+
+
+btn.addEventListener ("click", function (){
+    let index= Math.floor(Math.random()*5);
+    console.log(index)
+    demo.innerHTML = sayings[index].body;
+    author.innerHTML = sayings[index].author;
+})
+
